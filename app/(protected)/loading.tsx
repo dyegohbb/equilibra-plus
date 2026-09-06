9695277eb24269fd5a2536b06a7acf37d2eb9714
Dyego H. <dyegohbb@gmail.com>
2026-09-06 13:13:52 -0300

    Revert "corrige navegacao entre abas"
    This reverts commit 9da2612541e2333b901e98f80333c3c207c77b35.

--- app/(protected)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { LoadingState } from "@/components/ui/spinner";
-
-export default function ProtectedLoading() {
-  return <div className="full-page-loading"><LoadingState label="Abrindo sua área financeira…"/></div>;
-}
